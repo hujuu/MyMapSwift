cf4720158b0574e8409f3a701e138d26ede1ced8
hujuu <1961946+hujuu@users.noreply.github.com>
2023-11-15 10:28:22 +0900

    Create MapView.swift

---
+++ MyMap/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  MyMap
+//
+//  Created by KEN NAKAI on 2023/11/14.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    MapView()
+}
